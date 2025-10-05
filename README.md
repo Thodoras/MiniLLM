860f6d39d4eee2933fa465a26acbf8896971e2b4
Thodoras <theodoretopol@gmail.com>
2025-10-06 02:23:16 +0300

    Update readme

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ A light LLM model based on S. Raschka's book Build A Large Language Model.
 - Spliting text into word and subword tokens.his can be in a naive manner.  Alternatively apply advanced techniques such as byte pairing with the use of a third party library, namely tiktoken.
 - Sampling training examples with sliding window approach.
 - Convert tokens into vectors that feed the model.
+
+### Schema:
+Text -> Tokens -> TokenIds -> (Initially random) Embeddings + Positional Embeddings -> Input Embeddings
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-import tiktoken
+import torch
 
 from data_preparation.gpt_dataloader_v1 import create_dataloader_v1
 
 with open("source_data/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=4, shuffle=False)
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
+vocab_size = 50257
+output_dim = 256
+torch.manual_seed(123)
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
-second_batch = next(data_iter)
-print(second_batch)
+inputs, targets = next(data_iter)
+print(inputs)
+print(targets)
+
+token_embeddings = embedding_layer(inputs)
+print(token_embeddings.shape)
+
+#Create positional embedding layer
+context_length = 4
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+# Needed to get tensor from embedding.
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+input_embeddings = token_embeddings + pos_embeddings
